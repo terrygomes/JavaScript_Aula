ed4135337b4e24fc90281f8315b49f873271eea4
terrygomes <leandrogomes.santos>
2022-07-21 04:05:01 -0300

    Aula de variável

--- Variavel/js/acao.js
+++ Variavel/js/acao.js
@@ -1,4 +1,11 @@
 function ligar(){
+    
+     var nome ="Leandro"; // String
+     var idade = 38; // Number
+     var casado = false; // boolean
+      
+     alert(nome + " qual sua idade? " + idade + " voce e casado? " + casado);     
+
         document.getElementById("curso").src = "img/acesa.png";
 
 }
